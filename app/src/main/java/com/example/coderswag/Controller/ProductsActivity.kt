1a65e87537ec7521f130c46a98bb3ff5a84f4722
tjovcheska <jovcheskateodora@gmail.com>
2021-12-21 15:22:03 +0100

    07

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,15 +1,37 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType!!))
+        productListView.adapter = adapter
+
+        //1 portrait; 2 landscape
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
     }
 }
